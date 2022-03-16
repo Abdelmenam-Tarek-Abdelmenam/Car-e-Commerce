0575446f69f1083166895202aa179f7bfeabe74c
Abdelmenam-Tarek-Abdelmenam <moneam.elbahy@gmail.com>
2022-03-16 18:11:49 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Car E-Commerce',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

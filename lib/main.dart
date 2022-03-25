735023ea45f33a5b78e8cb801827b391222661c0
Yassin Wael <yasin162001@gmail.com>
2022-03-25 16:23:00 +0200

    test main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:car_e_commerce/constants/theme.dart';
 import 'package:car_e_commerce/data/local/pref_repository.dart';
 import 'package:car_e_commerce/data/local/sql_database.dart';
+import 'package:car_e_commerce/ui/screens/main_screen/main_screen.dart';
 import 'package:car_e_commerce/ui/screens/starting_screen/on_boarding/on_boarding_page.dart';
 import 'package:catcher/core/catcher.dart';
 import 'package:catcher/model/catcher_options.dart';
@@ -82,7 +83,7 @@ class AppView extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: lightThemeData,
 
-        home: const OnBoardingScreen(),
+        home: MainScreen(),
         // home: FlowBuilder<AuthStatus>(
         //   state: context.select((AuthStatusBloc bloc) => bloc.state.status),
         //   onGeneratePages: routes,
